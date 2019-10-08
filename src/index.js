3654bcb294d7fd63943dd348f495acea7768df65
Reginaldo <reginaldolribeiro@gmail.com>
2019-10-07 22:41:59 -0300

    Removing code comments and improvements in .env.example

--- src/index.js
+++ src/index.js
@@ -6,7 +6,6 @@ const swaggerUi = require('swagger-ui-express');
 const swaggerDocument = require('../src/swagger.json')
 
 const app = express()
-//{ useFindAndModify: false }
 
 mongoose.connect(process.env.APP_DB_CONNECTION, {
     useNewUrlParser: true,
@@ -14,11 +13,6 @@ mongoose.connect(process.env.APP_DB_CONNECTION, {
 })
 .then(conn => console.log("Connected to MongoDB Database!"))
 .catch(err => console.log("Error connecting to MongoDB Database!"))
-
-// mongoose.connect("mongodb+srv://" + process.env.APP_DB_CONNECTION,
-//     { useFindAndModify: false })
-//     .then(conn => console.log("Connected to MongoDB Database!"))
-//     .catch(err => console.log("Error connecting to MongoDB Database!"))
 
 app.use('/swagger', swaggerUi.serve, swaggerUi.setup(swaggerDocument))
 
